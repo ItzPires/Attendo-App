4c5c61739d82cf405e94226ccee103d4420b3dfd
Samuel Pires <samuel4534@live.com.pt>
2021-12-07 12:41:58 +0000

    Login Page :D

--- FrontEnd/flutterDep/lib/pages/login_page.dart
+++ FrontEnd/flutterDep/lib/pages/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -7,13 +8,80 @@ class LoginPage extends StatefulWidget {
   State<LoginPage> createState() => _LoginPageState();
 }
 
-/// This is the private State class that goes with MyStatefulWidget.
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('')),
-      extendBody: false,
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 60.0),
+              child: Center(
+                child: SizedBox(
+                    width: 200,
+                    height: 150,
+                    child: SvgPicture.asset(
+                      'assets/images/logo.svg',
+                      clipBehavior: Clip.antiAlias,
+                      color: const Color(0xFF000000),
+                    )),
+              ),
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Email',
+                ),
+              ),
+            ),
+            const Padding(
+              padding:
+                  EdgeInsets.only(left: 15.0, right: 15.0, top: 15, bottom: 0),
+              //padding: EdgeInsets.symmetric(horizontal: 15),
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(), labelText: 'Password'),
+              ),
+            ),
+            ElevatedButton(
+              style: ButtonStyle(
+                shadowColor: MaterialStateProperty.all<Color>(Colors.black),
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.black),
+              ),
+              onPressed: () {
+                // ignore: avoid_print
+                print("Press");
+              },
+              child: const Text(
+                'Login',
+                style: TextStyle(color: Colors.white, fontSize: 25),
+              ),
+            ),
+            const SizedBox(
+              height: 130,
+            ),
+            TextButton(
+              onPressed: () {},
+              child: const Text(
+                'Criar Conta',
+                style: TextStyle(color: Colors.black, fontSize: 15),
+              ),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: const Text(
+                'Recuperar Password',
+                style: TextStyle(color: Colors.black, fontSize: 15),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
