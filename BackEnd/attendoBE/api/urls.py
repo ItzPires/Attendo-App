9915acd619d2ee9b10c0abd5894a1387152ca17b
Samuel Pires <samuel4534@live.com.pt>
2021-12-22 19:27:07 +0000

    urls

--- BackEnd/attendoBE/api/urls.py
+++ BackEnd/attendoBE/api/urls.py
@@ -9,6 +9,9 @@ urlpatterns = [
     path('Teacher/', Calls.teacher.ProfessorViews.professor_manage),
     path('Teacher/<int:id>', Calls.teacher.ProfessorViews.teacher_search),
     path('Subject/', Calls.subject.SubjectViews.subject_manage),
+    path('Subject/', Calls.subject.SubjectViews.subject_manage),
+    path('Subject/<slug:emailStudent>', Calls.subject.SubjectViews.lecture_from_student),
+    path('Subject/<slug:emailTeacher>', Calls.subject.SubjectViews.lecture_from_teacher),
     path('login/', Calls.authentication.Authentication.user_login)
     #path('Subject/<>'Calls.subject.SUbjectViews.subject_search)
 ]
