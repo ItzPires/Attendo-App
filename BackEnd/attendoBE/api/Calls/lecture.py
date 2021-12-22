4e8782bd13bf8b347baa1c1d5be495e73d671767
Samuel Pires <samuel4534@live.com.pt>
2021-12-22 19:21:53 +0000

    o flutter já vem

--- BackEnd/attendoBE/api/Calls/lecture.py
+++ BackEnd/attendoBE/api/Calls/lecture.py
@@ -63,3 +63,52 @@ class LectureViews():
                     return JsonResponse({'sucesso': 1})
                 except DatabaseError:
                     return JsonResponse(error("Unable to update lecture information"))
+
+    @api_view(http_method_names=["GET"])
+    def lecture_from_student(request, emailStudent):
+        #Get lecture with this number
+        with connection.cursor() as cursor:
+            if(request.method == "GET"):
+                cursor.execute("""SELECT * FROM aluno_turma WHERE aluno_utilizador_mail = %s""", (emailStudent, ))
+                data = cursor.fectchall() #turma_id - onde o aluno esta
+
+                response = []
+
+                for turma in data:
+                    cursor.execute("""SELECT * FROM horario_semanal WHERE turma_id = %s""", (turma["turma_id"], ))
+                    classes = cursor.fectchall() #horarios da aula da turma
+
+                    cursor.execute("""SELECT cadeira_id FROM turma WHERE id = %s""", (turma["turma_id"], ))
+                    cadeiraId = cursor.fectchall() #id da cadeira
+
+                    cursor.execute("""SELECT nome FROM cadeira WHERE id = %s""", (cadeiraId, ))
+                    cadeiraName = cursor.fectchall() #nome da cadeira
+
+                    for horario in classes:
+                        to_add = {"aula": cadeiraName, "dia_de_semana": horario["dia_de_semana"], "hora_de_inicio": horario["hora_de_inicio"], "hora_de_fim": horario["hora_de_fim"]}
+                        response.append(to_add)
+
+                return Response(response)
+
+    @api_view(http_method_names=["GET"])
+    def lecture_from_teacher(request, emailTeacher):
+        #Get lecture with this number
+        with connection.cursor() as cursor:
+            if(request.method == "GET"):
+                cursor.execute("""SELECT * FROM turma WHERE professor_utilizador_mail = %s""", (emailTeacher, ))
+                data = cursor.fectchall() #turma - onde o stor esta
+
+                response = []
+
+                for turma in data:
+                    cursor.execute("""SELECT * FROM horario_semanal WHERE turma_id = %s""", (turma["turma_id"], ))
+                    classes = cursor.fectchall() #horarios da aula da turma
+
+                    cursor.execute("""SELECT nome FROM cadeira WHERE id = %s""", (turma["cadeira_id"], ))
+                    cadeiraName = cursor.fectchall() #nome da cadeira
+
+                    for horario in classes:
+                        to_add = {"aula": cadeiraName, "dia_de_semana": horario["dia_de_semana"], "hora_de_inicio": horario["hora_de_inicio"], "hora_de_fim": horario["hora_de_fim"]}
+                        response.append(to_add)
+
+                return Response(response)
